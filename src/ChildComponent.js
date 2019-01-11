35663b1e136158ce192023eb5d628ef95e0c8c02
Charles Vazac <cvazac@akamai.com>
2019-01-11 09:59:40 -0600

    prove out isNativeElement

--- src/ChildComponent.js
+++ src/ChildComponent.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent } from 'react'
 
 window.__renderCount = 0
 class Child extends PureComponent {
@@ -6,8 +6,8 @@ class Child extends PureComponent {
     window.__renderCount++
     return (
       <p/>
-    );
+    )
   }
 }
 
-export default Child;
+export default Child
--- src/PatternComponent.js
+++ src/PatternComponent.js
@@ -2,10 +2,28 @@ import React, { PureComponent } from 'react';
 import ChildComponent from './ChildComponent';
 
 class PatternComponent extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+  componentWillUpdate() {
+    window._dur = -1 * performance.now()
+  }
+  componentDidUpdate() {
+    window._dur += performance.now()
+    console.info('duration', window._dur)
+  }
   render() {
+    var markup = []
+    for (let i = 0; i < 100000; i++) {
+      markup.push(<span prop1={{}} key={i} />)
+    }
+
     return (
-      <ChildComponent prop={{}} />
-    );
+        <div>
+          <!--ChildComponent prop={{}} /-->
+          {markup.map(m => m)}
+        </div>
+    )
   }
 }
 
